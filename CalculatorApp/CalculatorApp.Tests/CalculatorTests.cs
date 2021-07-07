7b715a72bb74958a15f55a1bf4bb22e581e51a77
Paul Michaels <pcmichaels@live.com>
2021-07-07 07:58:02 +0100

    Add calculator app and tests

---
+++ CalculatorApp/CalculatorApp.Tests/CalculatorTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xunit;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Calculator_Add_ReturnsCorrect()
+        {
+            // Arrange            
+
+            // Act
+            decimal result = CalculatorApp.Calculator.Add(3, 6);
+
+            // Assert
+            Assert.Equal(9, result);
+        }
+
+        [Fact]
+        public void Calculator_Subtract_Correct()
+        {
+            // Arrange            
+
+            // Act
+            decimal result = CalculatorApp.Calculator.Subtract(1, 0);
+
+            // Assert
+            Assert.Equal(1, result);
+
+        }
+
+        [Fact]
+        public void Calculator_Subtract_Negative()
+        {
+            // Arrange            
+
+            // Act
+            decimal result = CalculatorApp.Calculator.Subtract(10, 20);
+
+            // Assert
+            Assert.Equal(-10, result);
+
+        }
+
+        [Fact]
+        public void Calculator_Multiply_Correct()
+        {
+            // Arrange            
+
+            // Act
+            decimal result = CalculatorApp.Calculator.Multiply(5, 2);
+
+            // Assert
+            Assert.Equal(10, result);
+
+        }
+         
+        [Fact]
+        public void Calculator_Divide_WillSurvive()
+        {
+            // Arrange            
+
+            // Act
+            decimal result = CalculatorApp.Calculator.Divide(7, 3);
+
+            // Assert
+            //Assert.Equal(2.5m, result);
+
+        }
+
+    }
+}
---
+++ CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    public static class Calculator
+    {
+        public static decimal Add(decimal x, decimal y) =>
+            x + y;
+
+        public static decimal Subtract(decimal x, decimal y) =>
+            x - y;
+
+        public static decimal Multiply(decimal x, decimal y) =>
+            x * y;
+
+        public static decimal Divide(decimal x, decimal y) =>
+            x / y;
+
+    }
+}
